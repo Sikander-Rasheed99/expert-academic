86295dfa706e12e0307f766a656b2217a42de552
Sikander-Rasheed99 <sikanderrasheed39@gmail.com>
2023-09-25 12:14:37 +0500

    Expert Academic Completed

---
+++ assets/js/script.js
@@ -0,0 +1,139 @@
+
+$('.client-slider').slick({
+  autoPlay: true,
+  speed: '1000',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  //  centerMode: true
+  arrows: false,
+  dots: false,
+});
+
+$('.brands-slider').slick({
+  autoPlay: true,
+  speed: '1000',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  //  centerMode: true
+  arrows: false,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+  ]
+});
+
+$(document).ready(function () {
+  $(".expert-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    autoplay: false
+  });
+  $(".prev-btn").click(function () {
+    $(".expert-slider").slick("slickPrev");
+  });
+
+  $(".next-btn").click(function () {
+    $(".expert-slider").slick("slickNext");
+  });
+  $(".prev-btn").addClass("slick-disabled");
+  $(".expert-slider").on("afterChange", function () {
+    if ($(".slick-prev").hasClass("slick-disabled")) {
+      $(".prev-btn").addClass("slick-disabled");
+    } else {
+      $(".prev-btn").removeClass("slick-disabled");
+    }
+    if ($(".slick-next").hasClass("slick-disabled")) {
+      $(".next-btn").addClass("slick-disabled");
+    } else {
+      $(".next-btn").removeClass("slick-disabled");
+    }
+  });
+});
+
+
+
+// input tel start
+
+
+var input = document.querySelector("#phone");
+var iti = window.intlTelInput(input, {
+  // separateDialCode:true,
+  utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.0/build/js/utils.js",
+});
+
+// store the instance variable so we can access it in the console e.g. window.iti.getNumber()
+window.iti = iti;
+
+// input tel end 
+
+
+// loader js start
+
+// Function to hide the loader
+function hideLoader() {
+  const loaderContainer = document.querySelector('.loader-container');
+  loaderContainer.style.display = 'none';
+}
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function () {
+  // Show the loader initially
+  const loaderContainer = document.querySelector('.loader-container');
+  loaderContainer.style.display = 'flex';
+
+  // After a delay of 3 seconds (adjust the value as needed)
+  // Call the function to hide the loader
+  setTimeout(hideLoader, 4000);
+});
+
+// loader js end 
+
+
+// faqs start
+
+
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+
+    const currentlyActiveAccordionItemHeader = document.querySelector(
+      ".accordion-item-header.active"
+    );
+    if (
+      currentlyActiveAccordionItemHeader &&
+      currentlyActiveAccordionItemHeader !== accordionItemHeader
+    ) {
+      currentlyActiveAccordionItemHeader.classList.toggle("active");
+      currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    }
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
+
+
+// faqs end 
